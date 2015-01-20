9af645cb28731ee2abbc9f53da9df3806098f529
Gustaf Cele <gustafc@gmail.com>
2015-01-20 22:09:54 +0100

    Test script for sorting

--- test_sort.sh
+++ test_sort.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+set -e
+
 cd `dirname $0`
 mvn install
 for cls in Sort01AnonClass Sort02FirstLambda Sort03NoTypes Sort04SlimmestLambda Sort05MethodReference ; do
     echo "--- $cls ---"
-    java -cp target/classes java8fp.$cls hello 123 world 45 6789
+    java -cp target/classes java8fp.syntax.$cls hello 123 world 45 6789
 done
     
