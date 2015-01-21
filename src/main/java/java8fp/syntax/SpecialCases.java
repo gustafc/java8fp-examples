4eee5046b7325f55c7a4140f1ed74dfdadd5371d
Gustaf Cele <gustafc@gmail.com>
2015-01-21 20:17:07 +0100

    more special cases

--- src/main/java/java8fp/syntax/SpecialCases.java
+++ src/main/java/java8fp/syntax/SpecialCases.java
@@ -1,13 +1,23 @@
 package java8fp.syntax;
 
 import java.awt.event.ActionListener;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class SpecialCases {
 
     public static void main(String[] args) {
         Runnable r = () -> System.out.println("Running!");
-        ActionListener a = e -> System.out.println(e.getActionCommand());
+        ActionListener a = e -> System.out.println(e);
+        // vs.
+        ActionListener b = System.out::println;
+
+        Function<String, Integer> lengthOfString = String::length;
+        assert lengthOfString.apply("hej") == 3;
+
+        Function<Long, Date> timestampToDate = Date::new;
     }
 
 }
